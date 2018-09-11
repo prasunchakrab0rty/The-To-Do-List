45bc03f4cbed1e6595032362e7d06f3e890c1fc8
lordprasun <lordprasun@gmail.com>
2018-09-11 18:28:35 +0530

    all about component and ngfor and ngif

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import {Hero} from './hero'
+export const heroes:Hero []=[
+    {id:11,name:'Batman'},
+    {id:12,name:'Superman'},
+    {id:13,name:'Flash'},
+    {id:14,name:'Aquaman'},
+    {id:15,name:'Wonderwomen'},
+]
