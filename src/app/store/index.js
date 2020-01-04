d91b0f8a988eb28d5876afdebfcb4877436ecaf3
Prasun Chakraborty <lordprasun@gmail.com>
2020-01-04 10:26:55 +0530

    Initial Commit

--- src/app/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-
-import { defaultState } from '../../server/defaultState'
-export const store = createStore(
-    function reducer(state = defaultState, action) {
-        return state
-    }
-)
