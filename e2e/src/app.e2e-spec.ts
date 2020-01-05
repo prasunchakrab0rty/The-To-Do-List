e13a57ef673362261ce60b9a8c6998323df5bdd3
Prasun Chakraborty <lordprasun@gmail.com>
2020-01-05 10:26:55 +0530

    Angular Material Initial Commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('agile-times app is running!');
+    expect(page.getTitleText()).toEqual('angularmaterial app is running!');
   });
 
   afterEach(async () => {
